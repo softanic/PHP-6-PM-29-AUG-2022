823b5da9e6795af6017eebd65331cee98cd3d9bb
softanic <info@softanic.in>
2022-11-15 18:49:53 +0530

    php

---
+++ php6pm/php6pm.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<h1>welcome</h1>
+		
+		<?php
+			define('NAME','himanshu',true);
+			
+		#	echo name;
+			
+			
+			$lang='java';
+			//type($lang);
+			/*
+			echo $lang;
+			echo "$lang";
+			echo '$lang';
+		
+			echo "<br>welcome to {$lang}<br>";
+			print('<br>this is print<br>');
+			echo "$lang  is easy<br>";
+			$lang='html';
+			echo $lang. '  is used for web development<br>';
+			echo 'welcome to '.$lang.'<br>';
+			
+		*/
+		?>
+	</body>
+</html>
