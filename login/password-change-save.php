e86148774dd2b2a4258bcf8c2ec6a2a3ce663043
softanic <info@softanic.in>
2022-12-09 18:44:58 +0530

    change password

--- login/password-change-save.php
+++ login/password-change-save.php
@@ -5,8 +5,8 @@ $user_id=$user['user_id'];
 
 
 $old=md5($_POST['old_password']);
-$new=$_POST['new_password'];
-$confirm=$_POST['confirm_password'];
+$new=md5($_POST['new_password']);
+$confirm=md5($_POST['confirm_password']);
 
 $conn=mysqli_connect('localhost','root','','php6pm');
 $q="select * from users where user_id='$user_id' and password='$old'";
@@ -18,15 +18,25 @@ if($count>0)
 	if($new==$confirm)
 	{
 		//update password
-		
+		$q2="update users set password='$new' where user_id='$user_id'";
+		$res2=mysqli_query($conn,$q2);
+		if(mysqli_affected_rows($conn))
+		{
+			//password changed successfully
+			echo "<script>alert('password changed successfully'); window.location='home.php';</script>";
+		}else{
+			echo "<script>alert('something went wrong, Please try again.'); window.history.back();</script>";
+		}
 	}else{
 		
 		//error: new password and confirm_password doesnt match 
+		echo "<script>alert('New pass and confirm password doesnt match, Please try again.');window.history.back();</script>";
 	}
 	
 }else{
 	
 	//error: old password incorrect
+	echo "<script>alert('Old password is wrong, Please try again.'); window.history.back();</script>";
 }
 
 ?>
--- login/password-change.php
+++ login/password-change.php
@@ -1,12 +1,12 @@
 <html>
 	<body>
-		<form a	ction="password-change-save.php" method="post">
+		<form action="password-change-save.php" method="post">
 			<input type="text" name="old_password" placeholder="enter old password" required><br><br>
 			<input type="text" name="new_password" placeholder="enter new password"  required><br><br>
 			<input type="text" name="confirm_password" placeholder="enter new password again"  required><br><br>
 			<input type="submit" value="Change my password" ><br>
 			
-			
+			<?php echo md5('123456'); ?>
 			
 		</form>
 	</body>
